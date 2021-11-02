7b56ee4da6c6c7ba19feae746200764b7768729c
Ines-ux-dev <ines.ouacif2@gmail.com>
2021-11-02 14:31:20 +0100

    Creation of InesBranch

---
+++ frontend/src/components/nextDates.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+export class nextDates extends React.Component {
+    render() {
+        return
+        (
+            <div className="mt-5 d-flex justify-content-left">
+                <h3>Les prochaines dates</h3>
+            </div>
+
+        )
+    }
+}
