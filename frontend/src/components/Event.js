73d1c34ed9d3be88f1fa8e269f09a35e466dc723
Ines-ux-dev <ines.ouacif2@gmail.com>
2021-11-12 12:46:56 +0100

    Events component

---
+++ frontend/src/components/Event.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../event.css'
+
+export class Event extends Component {
+    render() {
+        return (
+            <div className="line-event">
+                <table>
+                    <td>
+                        bonjour
+                    </td>
+                    <td>
+                        jbkjb
+                    </td>
+                </table>
+            </div>
+        )
+    }
+
+}
+
