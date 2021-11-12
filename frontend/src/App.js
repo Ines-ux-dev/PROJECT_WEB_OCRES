331c1db26c3a38a0583de3380cba4ccb85626004
Charlene167 <charlene.corencin@edu.ece.fr>
2021-11-12 11:38:00 +0100

    Modif Cha

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,37 @@
 import React from 'react';
-/*import logo from './logo.svg';*/
+import logo from './logo.svg';
 import './App.css';
 
-import { nextDates } from './components/nextDates'
-/*import { test } from './components/test'*/
+import { Home } from './components/Home'
+
 
 function App() {
   return (
 
     <div className="container">
-      <nextDates />
+      <Home />
     </div>
 
-    /*
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            
-            
-    
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>*/
+
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
   );
 }
 
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div className = "mt-5 d-flex justify-content-left">
+                <h3>Bienvenue, c'est la page de garde</h3>
+            </div>
+        )
+    }
+}
